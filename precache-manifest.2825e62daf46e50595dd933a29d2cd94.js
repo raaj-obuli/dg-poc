03d3f92cf15cd476b81a2a83ccfa48e1b6e25740
sudmon200 <to.sudipmondal@gmail.com>
2020-09-19 16:27:03 +0530

    Updates

---
+++ precache-manifest.2825e62daf46e50595dd933a29d2cd94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df93ad92004e499543b9f532ad661760",
+    "url": "/dg-poc/index.html"
+  },
+  {
+    "revision": "d2a3a1b8ac6ecae0c866",
+    "url": "/dg-poc/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "eddc3e090c69a00b625e",
+    "url": "/dg-poc/static/css/main.5a9d90de.chunk.css"
+  },
+  {
+    "revision": "d2a3a1b8ac6ecae0c866",
+    "url": "/dg-poc/static/js/2.4bf48a8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dg-poc/static/js/2.4bf48a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eddc3e090c69a00b625e",
+    "url": "/dg-poc/static/js/main.e7d48705.chunk.js"
+  },
+  {
+    "revision": "d34c1413a3bef6d6e3da",
+    "url": "/dg-poc/static/js/runtime-main.783250ed.js"
+  }
+]);
--- src/actions/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const GET_MOVIE_LIST = 'GET_MOVIE_LIST';
-export const SEARCH = 'SEARCH';
